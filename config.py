caf76ab8ef6ce741bba1506644b4cb3f22e5d9fb
Swarangi <shirsekarswarangi@gmail.com>
2024-11-10 17:31:52 +0530

    Remove sensitive files from tracking

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-import os
-
-
-class Config:
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/eventdb')
-    SECRET_KEY = os.getenv('SECRET_KEY', '6692a191b7c75f139ddcea9dfc7d1c8f')
